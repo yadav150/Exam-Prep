4ab8b46f76bc6200a0d5b42d41748fd9d9e1b24a
Yadav Subba <yadavsubba2003@gmail.com>
2026-04-10 21:01:51 +0530

    Update quiz.js

--- js/quiz.js
+++ js/quiz.js
@@ -1,1 +1,63 @@
-console.log("Quiz page loaded");
+let questions = [];
+let currentQuestion = 0;
+let score = 0;
+let selectedCategory = localStorage.getItem("quizCategory");
+
+// Fetch questions
+fetch("data/questions.json")
+  .then(res => res.json())
+  .then(data => {
+    // Filter by category
+    questions = data.filter(q => q.category === selectedCategory);
+
+    if (questions.length === 0) {
+      document.getElementById("question").innerText = "No questions available";
+      return;
+    }
+
+    loadQuestion();
+  });
+
+function loadQuestion() {
+  let q = questions[currentQuestion];
+
+  document.getElementById("question").innerText = q.question;
+
+  let options = document.querySelectorAll(".option");
+
+  options.forEach((btn, index) => {
+    btn.innerText = q.options[index];
+
+    btn.onclick = () => {
+      checkAnswer(btn.innerText);
+    };
+  });
+}
+
+function checkAnswer(selected) {
+  let correct = questions[currentQuestion].answer;
+
+  if (selected === correct) {
+    score++;
+  }
+
+  nextQuestion();
+}
+
+function nextQuestion() {
+  currentQuestion++;
+
+  if (currentQuestion < questions.length) {
+    loadQuestion();
+  } else {
+    // Save score
+    localStorage.setItem("quizScore", score);
+    localStorage.setItem("totalQuestions", questions.length);
+
+    // Go to result page
+    window.location.href = "result.html";
+  }
+}
+
+// Next button
+document.getElementById("nextBtn").onclick = nextQuestion;
